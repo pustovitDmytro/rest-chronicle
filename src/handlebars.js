50d5d3b74de4397928e410336ffd60cc9a62c03f
Dmytro Pustovit <dipustovit@gmail.com>
2020-07-28 22:52:37 +0300

    Chore: improves coverage

--- src/handlebars.js
+++ src/handlebars.js
@@ -21,19 +21,19 @@ handleBars.registerHelper('findById', (map, id) => {
 });
 
 handleBars.registerHelper({
-    eq  : (v1, v2) => v1 === v2,
-    ne  : (v1, v2) => v1 !== v2,
-    lt  : (v1, v2) => v1 < v2,
-    gt  : (v1, v2) => v1 > v2,
-    lte : (v1, v2) => v1 <= v2,
-    gte : (v1, v2) => v1 >= v2,
-    and() {
-        return Array.prototype.every.call(arguments, Boolean);
-    },
-    or() {
-        return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
-    },
-    isEmpty  : v => isEmpty(v),
+    // eq  : (v1, v2) => v1 === v2,
+    // ne  : (v1, v2) => v1 !== v2,
+    // lt  : (v1, v2) => v1 < v2,
+    // gt  : (v1, v2) => v1 > v2,
+    // lte : (v1, v2) => v1 <= v2,
+    // gte : (v1, v2) => v1 >= v2,
+    // and() {
+    //     return Array.prototype.every.call(arguments, Boolean);
+    // },
+    // or() {
+    //     return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+    // },
+    // isEmpty  : v => isEmpty(v),
     notEmpty : v => v && !isEmpty(v)
 });
 
