107854fe5fee92250ff85ce78b0fa3855162733b
Dmytro Pustovit <dipustovit@gmail.com>
2021-04-18 22:42:31 +0300

    Chore: (refactor) decrease technical debt

---
+++ src/reporters/utils.js
@@ -0,0 +1,26 @@
+import { isArray } from 'myrmidon';
+
+export function findGroup(obj, filters, id, index = 0) {
+    const { mergeArray } = this;
+
+    if (index === filters.length) {
+        if (mergeArray && isArray(obj)) return [ ...obj, id ];
+
+        return [ id ];
+    }
+    obj[filters[index]] = findGroup.call(  // eslint-disable-line no-param-reassign
+        this,
+        obj[filters[index]] || {},
+        filters,
+        id,
+        index + 1,
+    );
+
+    return obj;
+}
+
+export function detectType(value, extra = {}) {
+    if (extra.null && value === null) return extra.null;
+
+    return typeof value;
+}
