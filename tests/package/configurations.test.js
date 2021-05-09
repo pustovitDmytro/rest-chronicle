415b55704827ee749373a1e5715e976c372fa3c4
Dmytro Pustovit <dmytro.pustovit@gmail.com>
2021-05-10 01:20:32 +0300

    Chore: adds Default run test

--- tests/package/configurations.test.js
+++ tests/package/configurations.test.js
@@ -1,8 +1,16 @@
 import { assert } from 'chai';
-import entry from '../entry';
+import entry, { Chronicle } from '../entry';
+import { tmpFolder } from '../constants';
 
 suite('Configurations');
 
 test('Default configuration', function () {
     assert.exists(entry);
+});
+
+test('Default run', async function () {
+    assert.exists(Chronicle);
+    const chronicle = new Chronicle();
+
+    await chronicle.save(`${tmpFolder}/default.json`);
 });
